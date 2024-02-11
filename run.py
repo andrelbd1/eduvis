2b290bc28c6c43b9a0b3c941716e649d762bb962
Andre Damasceno <andrelbd1@gmail.com>
2024-02-11 00:32:08 -0300

    Update dir from app to src at run.py

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
-import os
-import sys
+# import os
+# import sys
 
-visualizations_dir = os.path.dirname(os.path.dirname(os.path.abspath("visualizations")))
-sys.path.append(visualizations_dir) # In case of error, put manually the path visualizations 
+# visualizations_dir = os.path.dirname(os.path.dirname(os.path.abspath("visualizations")))
+# sys.path.append(visualizations_dir) # In case of error, put manually the path visualizations 
 
-from app import app as application
+from src import app
+from src.config import settings
 # application.run(debug=True,port=8558,host='0.0.0.0')
 application.run(debug=True,port=8558)
+
+if __name__ == "__main__":
+    debug=False
+    if settings.ENV == "local":
+        debug=True
+    
+    # app.run(debug=True,port=8558)
+    app.run(host='0.0.0.0',port=8558,debug=debug)
